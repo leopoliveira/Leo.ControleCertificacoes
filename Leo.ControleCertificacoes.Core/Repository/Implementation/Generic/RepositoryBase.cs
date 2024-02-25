259903e64b1696415680526ef6409239b208c786
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-25 15:14:10 -0300

    Refactor: Making Base Repository methods virtual.

--- Leo.ControleCertificacoes.Core/Repository/Implementation/Generic/RepositoryBase.cs
+++ Leo.ControleCertificacoes.Core/Repository/Implementation/Generic/RepositoryBase.cs
@@ -17,33 +17,33 @@ namespace Leo.ControleCertificacoes.Core.Repository.Implementation.Generic
             _dbSet = context.Set<TEntity>();
         }
 
-        public async Task<TEntity> GetByCodeAsync(int code)
+        public virtual async Task<TEntity> GetByCodeAsync(int code)
         {
             return await _dbSet
                 .AsNoTracking()
                 .FirstOrDefaultAsync(entity => entity.Code == code);
         }
 
-        public async Task<TEntity> GetByIdAsync(Guid id)
+        public virtual async Task<TEntity> GetByIdAsync(Guid id)
         {
             return await _dbSet
                 .AsNoTracking()
                 .FirstOrDefaultAsync(entity => entity.Id == id);
         }
 
-        public async Task<int> CountAsyncAsync()
+        public virtual async Task<int> CountAsyncAsync()
         {
             return await _dbSet.CountAsync();
         }
 
-        public async Task<int> InsertAsync(TEntity entity)
+        public virtual async Task<int> InsertAsync(TEntity entity)
         {
             _dbSet.Add(entity);
 
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<int> UpdateAsync(TEntity entity)
+        public virtual async Task<int> UpdateAsync(TEntity entity)
         {
             _context.Entry(entity).State = EntityState.Modified;
             _context.Entry(entity).Property(entity => entity.Code).IsModified = false;
@@ -51,7 +51,7 @@ namespace Leo.ControleCertificacoes.Core.Repository.Implementation.Generic
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<int> DeleteAsync(TEntity entity)
+        public virtual async Task<int> DeleteAsync(TEntity entity)
         {
             _dbSet.Remove(entity);
 
