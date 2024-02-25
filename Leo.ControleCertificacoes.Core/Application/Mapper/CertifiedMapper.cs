215b05be792b7ff872d69feb038c7f0fb6a2b276
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-25 15:08:16 -0300

    Feat: Adding mappers.

---
+++ Leo.ControleCertificacoes.Core/Application/Mapper/CertifiedMapper.cs
@@ -0,0 +1,52 @@
+﻿using Leo.ControleCertificacoes.Core.Application.Dtos.Certified;
+using Leo.ControleCertificacoes.Core.Domain.Entities;
+
+namespace Leo.ControleCertificacoes.Core.Application.Mapper
+{
+    public static class CertifiedMapper
+    {
+        #region "Certified"
+
+        public static Certified ToCertified(this CertifiedCreateDto certified)
+        {
+            return new Certified
+            {
+                Name = certified.Name,
+                Expiration = certified.Expiration,
+                Description = certified.Description,
+                EmployeeId = certified.EmployeeId
+            };
+        }
+
+        #endregion
+
+        #region "CertifiedCreateDto"
+
+        public static CertifiedCreateDto ToCertifiedCreateDto(this Certified certified)
+        {
+            return new CertifiedCreateDto
+            {
+                Name = certified.Name,
+                Expiration = certified.Expiration,
+                Description = certified.Description,
+                EmployeeId = certified.EmployeeId
+            };
+        }
+
+        #endregion
+
+        #region "CertifiedReadDto"
+
+        public static CertifiedReadDto ToCertifiedReadDto(this Certified certified)
+        {
+            return new CertifiedReadDto
+            {
+                Name = certified.Name,
+                Expiration = certified.Expiration,
+                Description = certified.Description
+            };
+        }
+
+        #endregion
+    }
+}
