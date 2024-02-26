81fef780b6d8172d25706f97282106e5c187d6ac
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-26 06:24:30 -0300

    Feat: Model validation.

--- Leo.ControleCertificacoes.Core/Application/Dtos/Certified/CertifiedCreateDto.cs
+++ Leo.ControleCertificacoes.Core/Application/Dtos/Certified/CertifiedCreateDto.cs
@@ -1,13 +1,21 @@
-﻿namespace Leo.ControleCertificacoes.Core.Application.Dtos.Certified
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Leo.ControleCertificacoes.Core.Application.Dtos.Certified
 {
     public record CertifiedCreateDto
     {
+        [Required]
+        [StringLength(256)]
         public string Name { get; set; } = null!;
 
+        [Required]
+        [DataType(DataType.Date)]
         public DateOnly Expiration { get; set; }
 
+        [StringLength(256)]
         public string? Description { get; set; }
 
+        [Required]
         public Guid EmployeeId { get; set; }
     }
 }
--- Leo.ControleCertificacoes.Core/Application/Dtos/Employee/EmployeeCreateDto.cs
+++ Leo.ControleCertificacoes.Core/Application/Dtos/Employee/EmployeeCreateDto.cs
@@ -1,9 +1,14 @@
-﻿namespace Leo.ControleCertificacoes.Core.Application.Dtos.Employee
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Leo.ControleCertificacoes.Core.Application.Dtos.Employee
 {
     public record EmployeeCreateDto
     {
+        [Required]
+        [StringLength(256)]
         public string Name { get; set; } = null!;
 
+        [StringLength(256)]
         public string? Department { get; set; }
     }
 }
