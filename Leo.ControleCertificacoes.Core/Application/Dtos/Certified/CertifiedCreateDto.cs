f346288e71d2a30e6e17d640ff640d395d5bd28d
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-26 06:33:53 -0300

    Refactor: Error messages and display names.

--- Leo.ControleCertificacoes.Core/Application/Dtos/Certified/CertifiedCreateDto.cs
+++ Leo.ControleCertificacoes.Core/Application/Dtos/Certified/CertifiedCreateDto.cs
@@ -4,18 +4,22 @@ namespace Leo.ControleCertificacoes.Core.Application.Dtos.Certified
 {
     public record CertifiedCreateDto
     {
-        [Required]
-        [StringLength(256)]
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [StringLength(256, ErrorMessage = "O {0} deve ter um tamanho máximo de {1}.")]
+        [Display(Name = "Nome")]
         public string Name { get; set; } = null!;
 
-        [Required]
-        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [DataType(DataType.Date, ErrorMessage = "Informe um formato válido para Data.")]
+        [Display(Name = "Data de expiração")]
         public DateOnly Expiration { get; set; }
 
-        [StringLength(256)]
+        [StringLength(256, ErrorMessage = "O {0} deve ter um tamanho máximo de {1}.")]
+        [Display(Name = "Descrição")]
         public string? Description { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [Display(Name = "Id do Funcionário")]
         public Guid EmployeeId { get; set; }
     }
 }
--- Leo.ControleCertificacoes.Core/Application/Dtos/Employee/EmployeeCreateDto.cs
+++ Leo.ControleCertificacoes.Core/Application/Dtos/Employee/EmployeeCreateDto.cs
@@ -4,11 +4,13 @@ namespace Leo.ControleCertificacoes.Core.Application.Dtos.Employee
 {
     public record EmployeeCreateDto
     {
-        [Required]
-        [StringLength(256)]
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [StringLength(256, ErrorMessage = "O {0} deve ter um tamanho máximo de {1}.")]
+        [Display(Name = "Nome")]
         public string Name { get; set; } = null!;
 
-        [StringLength(256)]
+        [StringLength(256, ErrorMessage = "O {0} deve ter um tamanho máximo de {1}.")]
+        [Display(Name = "Departamento/Setor")]
         public string? Department { get; set; }
     }
 }
