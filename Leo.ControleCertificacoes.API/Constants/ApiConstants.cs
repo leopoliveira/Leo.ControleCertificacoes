87959227ed40b238df76f4b3bcc420aa2b06924a
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-26 07:02:07 -0300

    Refactor: Adding swagger configurations.

---
+++ Leo.ControleCertificacoes.API/Constants/ApiConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Leo.ControleCertificacoes.API.Constants
+{
+    public sealed class ApiConstants
+    {
+        public const string API_V1 = "v1";
+
+        public const string TITLE = "Controle de Certificações";
+        public const string DESCRIPTION = "API para Controle de Certificações de funcionários";
+        public const string AUTHOR = "Leonardo Oliveira";
+        public const string GITHUB_URL = "https://github.com/leopoliveira";
+    }
+}
--- Leo.ControleCertificacoes.API/Program.cs
+++ Leo.ControleCertificacoes.API/Program.cs
@@ -1,4 +1,9 @@
+using System.Reflection;
+
+using Leo.ControleCertificacoes.API.Constants;
 using Leo.ControleCertificacoes.Core.Application.DependencyInjection;
+
+using Microsoft.OpenApi.Models;
 
 namespace Leo.ControleCertificacoes.API
 {
@@ -13,7 +18,26 @@ namespace Leo.ControleCertificacoes.API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.Configure<RouteOptions>(opt => opt.LowercaseUrls = true);
+
+            builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc(ApiConstants.API_V1, new OpenApiInfo
+                {
+                    Version = ApiConstants.API_V1,
+                    Title = ApiConstants.TITLE,
+                    Description = ApiConstants.DESCRIPTION,
+                    Contact = new OpenApiContact
+                    {
+                        Name = ApiConstants.AUTHOR,
+                        Url = new Uri(ApiConstants.GITHUB_URL)
+                    }
+                });
+
+                string xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                opt.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+            });
 
             // DbContext Injection
             builder.Services.AddDbContextService();
