32eff608ade7fd183c7587b216d44ea79e33abe3
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-29 06:20:29 -0300

    Feat: Update package dependencies and import PrimeVue library

--- Front/Leo.ControleDeCertificacoes.UI/src/main.ts
+++ Front/Leo.ControleDeCertificacoes.UI/src/main.ts
@@ -1,6 +1,9 @@
 import './assets/main.css'
+import 'primevue/resources/themes/aura-light-green/theme.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {}).mount('#app');
