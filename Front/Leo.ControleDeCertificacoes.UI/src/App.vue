7c209913e0c8a0ed6f63e87ccd05769489ae4f1c
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-03-01 07:49:09 -0300

    Refactor: code formatting and dependencies

--- Front/Leo.ControleDeCertificacoes.UI/src/App.vue
+++ Front/Leo.ControleDeCertificacoes.UI/src/App.vue
@@ -1,17 +1,25 @@
-<script setup lang="ts">
-import { RouterLink } from 'vue-router'
+<script lang="ts">
+import { RouterLink } from "vue-router";
+import Cu from "./components/Cu.vue";
+
+export default {
+   components: {
+      RouterLink,
+      Cu
+   }
+};
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+   <header>
+      <div class="wrapper">
+         <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <Cu />
+         </nav>
+      </div>
+   </header>
 </template>
 
 <style scoped></style>
--- Front/Leo.ControleDeCertificacoes.UI/src/main.ts
+++ Front/Leo.ControleDeCertificacoes.UI/src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import 'primevue/resources/themes/aura-light-green/theme.css'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(PrimeVue, { ripple: true });
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
