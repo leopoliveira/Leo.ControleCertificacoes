cf1efa32e7b3816ca8379f36cf73178712807580
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-03-04 21:32:14 -0300

    Remove Axios import and configuration

--- Front/Leo.ControleCertificacoes.UI/src/main.ts
+++ Front/Leo.ControleCertificacoes.UI/src/main.ts
@@ -5,14 +5,10 @@ import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
 import Lara from "./presets/lara";
-import Axios from "axios";
 
 const app = createApp(App);
 
 app.use(PrimeVue, { unstyled: true, pt: Lara });
 app.use(router)
-
-app.config.globalProperties.$axios = Axios;
-Axios.defaults.baseURL = import.meta.env.VITE_API_BASE_DEV_URL;
 
 app.mount("#app");
