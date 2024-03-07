5b3a03031e769345b3819751fce2074aef00dd7b
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-03-07 06:41:09 -0300

    Update navigation styling in App.vue

--- Front/Leo.ControleCertificacoes.UI/src/App.vue
+++ Front/Leo.ControleCertificacoes.UI/src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <header>
     <div class="">
-      <nav>
+      <nav class="flex w-100 gap-2 m-2 p-2">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/employee">Funcionários</RouterLink>
       </nav>
